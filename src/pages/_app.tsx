0846046ddfe8cd348d6b383c69c5c78ebe354361
Maruf Bepary <bepary71@gmail.com>
2023-01-30 14:30:54 +0000

    Added simple documentation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+/**
+ * Represents the entire application
+ * @param param0 - every page and component is a child of this component
+ * @returns 
+ */
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
