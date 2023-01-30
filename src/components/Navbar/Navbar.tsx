0a7536bf00a6ffa279de8ac9def189b019861065
Maruf Bepary <bepary71@gmail.com>
2023-01-30 15:43:14 +0000

    Added logo - Main logo - Text logo which disappears on mobile sizes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,25 @@
- import React from 'react';
+ import { Flex, Image } from '@chakra-ui/react';
+import React from 'react';
  
  const Navbar:React.FC = () => {
 	
-	return <div>Navbar</div>
+	return (
+		<Flex bg="white" height="44px" padding="6px 12px">
+		  <Flex align="center">
+			<Image src="/images/logo.svg" height="30px" alt="Website logo"/>
+			
+			{/* When screen size is mobile, SVG bellow is not displayed */}
+			<Image
+			  src="/images/logo_text.svg"
+			  height="46px"
+			  display={{ base: "none", md: "unset" }} 
+			  alt="Website text logo"
+			/>
+		  </Flex>
+		  {/* <Directory/> 
+		  <SearchInput />
+		  <RightContent user={user}/> */}
+		</Flex>
+	  );
  }
  export default Navbar;
