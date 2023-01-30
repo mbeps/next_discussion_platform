2587c5a8440eb45f4fd34b71ae40cad36494a647
Maruf Bepary <bepary71@gmail.com>
2023-01-30 15:00:30 +0000

    Created universal Chakra theme for app - Fonts - Colour - Body properties

---
+++ src/chakra/theme.ts
@@ -0,0 +1,29 @@
+// 1. Import `extendTheme`
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./button";
+
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#FF0000", 
+    },
+  },
+  fonts: {
+    // Default font family
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200", // app background
+      },
+    }),
+  },
+  components: {
+    // UI components offered by Chakra
+  },
+});
