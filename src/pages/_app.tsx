3100f07e0f2cbd680a76e2fdad639d58316e1d9c
Maruf Bepary <bepary71@gmail.com>
2023-01-30 15:11:59 +0000

    Imported missing theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { theme } from "@/chakra/theme";
 import Layout from "@/components/Layout/Layout";
 
 /**
@@ -9,7 +10,7 @@ import Layout from "@/components/Layout/Layout";
  */
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
