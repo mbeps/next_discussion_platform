4506850d511761fc57b3c9d8e1fdb9c48fc0b8ac
Maruf Bepary <bepary71@gmail.com>
2023-01-31 14:05:06 +0000

    Automatically close modal when - Signing in - Logging in

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,4 +1,5 @@
 import { authModalState } from "@/atoms/authModalAtom";
+import { auth } from "@/firebase/clientApp";
 import {
   Divider,
   Flex,
@@ -10,13 +11,19 @@ import {
   ModalOverlay,
   Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState } from "recoil";
 import AuthInputs from "./AuthInputs";
 import OAuthButtons from "./OAuthButtons";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) handleClose();
+  }, [user]);
 
   const handleClose = () => {
     setModalState((prev) => ({
