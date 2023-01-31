4b948f8dde142c80533e54fa9f8e0236bbca7923
Maruf Bepary <bepary71@gmail.com>
2023-01-31 14:26:12 +0000

    Created separate logout button components

---
+++ src/components/Navbar/RightContent/LogOutButton.tsx
@@ -0,0 +1,21 @@
+import { auth } from '@/firebase/clientApp';
+import { Button } from '@chakra-ui/react';
+import { signOut } from 'firebase/auth';
+import React from 'react';
+
+const LogOutButton:React.FC = () => {
+	
+	return (
+		<Button
+            height="28px"
+            display={{ base: "none", md: "flex" }} // on mobile, this button is not displayed
+            width={{ base: "70px", md: "110px" }} // on mobile the width is 70px, on desktop 110px
+            mr={2}
+            ml={2}
+            onClick={() => signOut(auth)}
+          >
+            Log Out
+          </Button>
+	)
+}
+export default LogOutButton;
--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,9 +1,8 @@
 import AuthModal from "@/components/Modal/Auth/AuthModal";
-import { auth } from "@/firebase/clientApp";
-import { Button, Flex } from "@chakra-ui/react";
-import { signOut } from "firebase/auth";
+import { Flex } from "@chakra-ui/react";
 import React from "react";
 import AuthButtons from "./AuthButtons";
+import LogOutButton from "./LogOutButton";
 
 type RightContentProps = {
   user: any;
@@ -15,16 +14,7 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
       <AuthModal />
       <Flex justify="center" align="center">
         {user ? (
-          <Button
-            height="28px"
-            display={{ base: "none", md: "flex" }} // on mobile, this button is not displayed
-            width={{ base: "70px", md: "110px" }} // on mobile the width is 70px, on desktop 110px
-            mr={2}
-            ml={2}
-            onClick={() => signOut(auth)}
-          >
-            Log Out
-          </Button>
+          <LogOutButton />
         ) : (
           <AuthButtons />
         )}
