1583151962ff6df4707d73c98c46763ff9dbd92e
Maruf Bepary <bepary71@gmail.com>
2023-02-20 14:54:05 +0000

    Added missing about component to post submission page

--- src/pages/community/[communityId]/submit.tsx
+++ src/pages/community/[communityId]/submit.tsx
@@ -1,7 +1,9 @@
 import { communityState } from "@/atoms/communitiesAtom";
+import About from "@/components/Community/About";
 import PageContent from "@/components/Layout/PageContent";
 import NewPostForm from "@/components/Posts/NewPostForm";
 import { auth } from "@/firebase/clientApp";
+import useCommunityData from "@/hooks/useCommunityData";
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -9,7 +11,8 @@ import { useRecoilValue } from "recoil";
 
 const SubmitPostPage: React.FC = () => {
   const [user] = useAuthState(auth);
-  const communityStateValue = useRecoilValue(communityState);
+  // const communityStateValue = useRecoilValue(communityState);
+  const { communityStateValue } = useCommunityData();
   console.log(communityStateValue);
   return (
     <PageContent>
@@ -21,7 +24,11 @@ const SubmitPostPage: React.FC = () => {
         </Box>
         {user && <NewPostForm user={user} />}
       </>
-      <>{/* About */}</>
+      <>
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
     </PageContent>
   );
 };
