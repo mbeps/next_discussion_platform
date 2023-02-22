a9d30278c9c8eccad55c0a6f32c0b6156eb4a458
Maruf Bepary <bepary71@gmail.com>
2023-02-22 17:38:36 +0000

    Added documentation

--- src/hooks/useDirectory.tsx
+++ src/hooks/useDirectory.tsx
@@ -5,16 +5,26 @@ import {
   directoryMenuState,
 } from "@/atoms/directoryMenuAtom";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { IoPeopleCircleOutline } from "react-icons/io5";
 import { useRecoilState, useRecoilValue } from "recoil";
 
+/**
+ *
+ * @returns directoryState (DirectoryState) - object containing the current directory state
+ */
 const useDirectory = () => {
   const [directoryState, setDirectoryState] =
     useRecoilState(directoryMenuState);
   const router = useRouter();
   const communityStateValue = useRecoilValue(communityState);
 
+  /**
+   * Allows the user to select a menu item from the directory menu.
+   * If the user is already on the page that the menu item links to, then the menu will close.
+   * If the user is not on the page that the menu item links to, then the user will be redirected to the page.
+   * @param menuItem (DirectoryMenuItem) - object representing the menu item that was selected
+   */
   const onSelectMenuItem = (menuItem: DirectoryMenuItem) => {
     setDirectoryState((prev) => ({
       ...prev,
@@ -26,6 +36,10 @@ const useDirectory = () => {
     }
   };
 
+  /**
+   * Toggles the directory menu open or closed.
+   * If the menu is open, then the menu will close.
+   */
   const toggleMenuOpen = () => {
     setDirectoryState((prev) => ({
       ...prev,
@@ -37,6 +51,7 @@ const useDirectory = () => {
     const { currentCommunity } = communityStateValue;
 
     if (currentCommunity) {
+      // if the user is currently in a community
       setDirectoryState((prev) => ({
         ...prev,
         selectedMenuItem: {
