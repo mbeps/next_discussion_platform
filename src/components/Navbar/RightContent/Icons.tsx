adc967da942924ad37a238947bc6c9d2418d0007
Maruf Bepary <bepary71@gmail.com>
2023-02-22 19:57:20 +0000

    Added functionality - Can create post from navbar - Can go to project GitHub repo

--- src/components/Navbar/RightContent/Icons.tsx
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -1,21 +1,19 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import useCallCreatePost from "@/hooks/useCallCreatePost";
+import { Flex, Icon } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import React from "react";
-import { AddIcon } from "@chakra-ui/icons";
-import { Box, Flex, Icon } from "@chakra-ui/react";
-import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
+import { IconType } from "react-icons";
 import { GrAdd } from "react-icons/gr";
-import {
-  IoFilterCircleOutline,
-  IoNotificationsOutline,
-  IoVideocam,
-  IoVideocamOutline,
-} from "react-icons/io5";
+import { VscGithub } from "react-icons/vsc";
 
 type IconProps = {
-  as: React.ElementType;
+  icon: IconType;
   fontSize: number;
+  onClick?: () => void;
 };
 
-const IconItem: React.FC<IconProps> = ({ as: Icon, fontSize }) => {
+const IconItem: React.FC<IconProps> = ({ icon, fontSize, onClick }) => {
   return (
     <Flex
       mr={1.5}
@@ -26,13 +24,17 @@ const IconItem: React.FC<IconProps> = ({ as: Icon, fontSize }) => {
       _hover={{
         bg: "gray.200",
       }}
+      onClick={onClick}
     >
-      <Icon fontSize={fontSize} />
+      <Icon as={icon} fontSize={fontSize} />
     </Flex>
   );
 };
 
 const icons: React.FC = () => {
+  const router = useRouter();
+  const { onClick } = useCallCreatePost();
+
   return (
     <Flex>
       <Flex
@@ -42,16 +44,19 @@ const icons: React.FC = () => {
         borderRight="1px solid"
         borderColor="gray.200"
       >
-        <IconItem as={BsArrowUpRightCircle} fontSize={20} />
-
-        <IconItem as={IoFilterCircleOutline} fontSize={22} />
-
-        <IconItem as={IoVideocamOutline} fontSize={22} />
+        {/* <IconItem icon={BsArrowUpRightCircle} fontSize={20} /> */}
+        {/* <IconItem icon={IoFilterCircleOutline} fontSize={22} /> */}
+        <IconItem
+          icon={VscGithub}
+          fontSize={20}
+          onClick={() => {
+            router.push("https://github.com/mbeps/next_discussion_platform");
+          }}
+        />
       </Flex>
       <>
         {/* Always visible */}
-        <IconItem as={GrAdd} fontSize={20} />
-        <IconItem as={IoNotificationsOutline} fontSize={20} />
+        <IconItem icon={GrAdd} fontSize={20} onClick={onClick} />
       </>
     </Flex>
   );
