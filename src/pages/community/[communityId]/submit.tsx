676f13291424fb00b9d79936f32aa9c14ef99766
Maruf Bepary <bepary71@gmail.com>
2023-02-25 19:31:02 +0000

    Added authentication check before posting

--- src/pages/community/[communityId]/submit.tsx
+++ src/pages/community/[communityId]/submit.tsx
@@ -1,33 +1,48 @@
-import { communityState } from "@/atoms/communitiesAtom";
+import { authModalState } from "@/atoms/authModalAtom";
 import About from "@/components/Community/About";
 import PageContent from "@/components/Layout/PageContent";
+import AuthButtons from "@/components/Navbar/RightContent/AuthButtons";
 import NewPostForm from "@/components/Posts/NewPostForm";
 import { auth } from "@/firebase/clientApp";
 import useCommunityData from "@/hooks/useCommunityData";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilValue } from "recoil";
+import { useSetRecoilState } from "recoil";
 
 const SubmitPostPage: React.FC = () => {
   const [user] = useAuthState(auth);
   // const communityStateValue = useRecoilValue(communityState);
   const { communityStateValue } = useCommunityData();
-  console.log(communityStateValue);
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <PageContent>
       <>
-        <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+        <Box p="14px 0px">
           <Text fontSize="20pt" fontWeight={700} color="black">
             Create Post
           </Text>
         </Box>
-        {user && (
+        {user ? (
           <NewPostForm
             user={user}
             communityImageURL={communityStateValue.currentCommunity?.imageURL}
             currentCommunity={communityStateValue.currentCommunity}
           />
+        ) : (
+          <Stack
+            justifyContent="center"
+            align="center"
+            bg="white"
+            p={5}
+            borderRadius={10}
+          >
+            <Text fontWeight={600}>Log in or sign up to post</Text>
+            <Stack direction="row" spacing={2} ml={4}>
+              <AuthButtons />
+            </Stack>
+          </Stack>
         )}
       </>
       <>
