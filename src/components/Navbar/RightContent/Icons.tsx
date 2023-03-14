80c1d2a1022da67ba8522988e87c9f04290f5008
Maruf Bepary <bepary71@gmail.com>
2023-03-14 18:47:40 +0000

    Created separate icon component - This component will be used by the community header

--- src/components/Navbar/RightContent/Icons.tsx
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+import IconItem from "@/components/atoms/Icon";
 import useCallCreatePost from "@/hooks/useCallCreatePost";
 import { Flex, Icon } from "@chakra-ui/react";
 import { useRouter } from "next/router";
@@ -6,44 +7,6 @@ import React from "react";
 import { IconType } from "react-icons";
 import { GrAdd } from "react-icons/gr";
 import { VscGithub } from "react-icons/vsc";
-
-/**
- * @param {IconType} icon - Icon to be displayed
- * @param {number} fontSize - Font size of the icon
- * @param {() => void} onClick - Function to be executed when the button is clicked
- */
-type IconProps = {
-  icon: IconType;
-  fontSize: number;
-  onClick?: () => void;
-};
-
-/**
- * Displays an icon with a hover effect.
- * Takes some props to render the icon and to execute a function when the icon is clicked.
- * @param {IconType} icon - Icon to be displayed
- * @param {number} fontSize - Font size of the icon
- * @param {() => void} onClick - Function to be executed when the button is clicked
- *
- * @returns {React.FC<IconProps>} - Icon with a hover effect
- */
-const IconItem: React.FC<IconProps> = ({ icon, fontSize, onClick }) => {
-  return (
-    <Flex
-      mr={1.5}
-      ml={1.5}
-      padding={1}
-      cursor="pointer"
-      borderRadius={4}
-      _hover={{
-        bg: "gray.200",
-      }}
-      onClick={onClick}
-    >
-      <Icon as={icon} fontSize={fontSize} />
-    </Flex>
-  );
-};
 
 /**
  * Displays icons in the right side of the navbar:
---
+++ src/components/atoms/Icon.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { Flex, Icon } from "@chakra-ui/react";
+import React from "react";
+import { IconType } from "react-icons";
+
+/**
+ * @param {IconType} icon - Icon to be displayed
+ * @param {number} fontSize - Font size of the icon
+ * @param {() => void} onClick - Function to be executed when the button is clicked
+ */
+type IconProps = {
+  icon: IconType;
+  fontSize: number;
+  onClick?: () => void;
+};
+
+/**
+ * Displays an icon with a hover effect.
+ * Takes some props to render the icon and to execute a function when the icon is clicked.
+ * @param {IconType} icon - Icon to be displayed
+ * @param {number} fontSize - Font size of the icon
+ * @param {() => void} onClick - Function to be executed when the button is clicked
+ *
+ * @returns {React.FC<IconProps>} - Icon with a hover effect
+ */
+const IconItem: React.FC<IconProps> = ({ icon, fontSize, onClick }) => {
+  return (
+    <Flex
+      mr={1.5}
+      ml={1.5}
+      padding={1}
+      cursor="pointer"
+      borderRadius={4}
+      _hover={{
+        bg: "gray.200",
+      }}
+      onClick={onClick}
+    >
+      <Icon as={icon} fontSize={fontSize} />
+    </Flex>
+  );
+};
+
+export default IconItem;
