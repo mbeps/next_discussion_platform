2242133a05d9f9f414b688a559022cd7ee1746e7
Maruf Bepary <bepary71@gmail.com>
2025-11-22 13:36:33 +0000

    Upgraded ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  },
+  {
+    ignores: [".next/"],
+  },
+];
+
+export default eslintConfig;
